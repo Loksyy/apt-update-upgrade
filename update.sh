a135c900f617c2152c3d7c48cb40055b4d5545b6
Loksyy <74905465+Loksyy@users.noreply.github.com>
2022-10-16 15:05:04 +0100

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+TEXT_JUMP='\e[0m'
+TEXT_GREEN='\033[0;32m'
+TEXT_RED_B='\e[1;31m'
+
+sudo apt update
+echo -e $TEXT_GREEN
+echo 'APT update finished...'
+echo -e $TEXT_JUMP
+
+sudo apt dist-upgrade
+echo -e $TEXT_GREEN
+echo 'APT distributive upgrade finished...'
+echo -e $TEXT_JUMP
+
+sudo apt upgrade
+echo -e $TEXT_GREEN
+echo 'APT upgrade finished...'
+echo -e $TEXT_JUMP
+
+sudo apt autoremove
+echo -e $TEXT_GREEN
+echo 'APT auto remove finished...'
+echo -e $TEXT_JUMP
+
+if [ -f /var/run/reboot-required ]; then
+    echo -e $TEXT_RED_B
+    echo 'Reboot required!'
+    echo -e $TEXT_RESET
+    echo -e $TEXT_RED_B
+    read -p "Are you sure? (y/n)" CONT
+    if [ "$CONT" = "y" ]; then
+    /sbin/reboot;
+    fi
+fi
